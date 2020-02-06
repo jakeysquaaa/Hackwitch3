ce8d743036f0fb3bfb3193126c9c1585f45fb4c6
CM Student <cmstudent@uhwo-acm-11.local>
2020-02-06 10:51:37 -1000

    I completed both problem sets.

--- Hackwitch3/ViewController.swift
+++ Hackwitch3/ViewController.swift
@@ -15,26 +15,47 @@ var firstString = "The background color will turn blue"
     
     var secondString = "The background color will turn green"
     
+  
+    @IBOutlet weak var redlabel: UILabel!
     
     
+    @IBOutlet weak var bluelabel: UILabel!
     
+    @IBOutlet weak var greenLabel: UILabel!
     
+    var thirdString = "The background will turn green"
+    var fourthString = "The background will turn green"
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func MagicButton(_ sender: Any) {
+        
+        //need to write a conditional statement that is true
+        
+        if thirdString == fourthString
+        {
+           //now you write the instruction here
+             self.view.backgroundColor = UIColor.green
+        }
+        
+        
+    }
+    
     @IBAction func changeButtonpress(_ sender: Any)
     {
     
         if firstString == "The background color will turn red"
     {
         self.view.backgroundColor = UIColor.red
-        
+        self.redlabel.text="Red"
     }
     else
     {
     self.view.backgroundColor = UIColor.blue
+        self.bluelabel.text="Blue"
     }
 
 }
